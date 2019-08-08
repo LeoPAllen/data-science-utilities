eefbaa07f7142c62625b4369e4a23a44af9b3319
LeoPAllen <leopallen@gmail.com>
2019-08-08 11:36:00 -0500

    Adding basic directory structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='datascienceutils',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/LeoPAllen/datascienceutils',
+      author='Leo Allen',
+      author_email='leopallen@gmail.com',
+      license='MIT',
+      packages=['datascienceutils'],
+      zip_safe=False)
